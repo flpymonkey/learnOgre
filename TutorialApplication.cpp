f8e53397aa7c2682e89056b12f9b950eda97895a
flpymonkey <bencj@cs.utexas.edu>
2018-09-03 12:07:02 -0500

    first commit

---
+++ TutorialApplication.cpp
@@ -0,0 +1,92 @@
+/*
+-----------------------------------------------------------------------------
+Filename:    TutorialApplication.cpp
+-----------------------------------------------------------------------------
+
+This source file is part of the
+   ___                 __    __ _ _    _
+  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
+/ \_// (_| | | |  __/  \  /\  /| |   <| |
+\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+      |___/
+Tutorial Framework (for Ogre 1.9)
+http://www.ogre3d.org/wiki/
+-----------------------------------------------------------------------------
+*/
+
+#include "TutorialApplication.h"
+#include "OgreColourValue.h"
+
+//---------------------------------------------------------------------------
+TutorialApplication::TutorialApplication(void)
+{
+}
+//---------------------------------------------------------------------------
+TutorialApplication::~TutorialApplication(void)
+{
+}
+
+//---------------------------------------------------------------------------
+void TutorialApplication::createScene(void)
+{
+    // Create your scene here :)
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+    Ogre::Light* light = mSceneMgr->createLight("MainLight");
+    Ogre::SceneNode* lightNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    lightNode->attachObject(light);
+
+    lightNode->setPosition(20, 80, 50);
+
+
+    Ogre::SceneNode* camNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    
+    mCamera->setNearClipDistance(5); // specific to this sample
+    mCamera->setAutoAspectRatio(true);
+    camNode->attachObject(mCamera);
+    camNode->setPosition(0, 0, 140);
+
+    Ogre::SceneNode* ogreNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    Ogre::Entity* ogreEntity = mSceneMgr->createEntity("ogrehead.mesh");
+    ogreNode->attachObject(ogreEntity);
+
+}
+//---------------------------------------------------------------------------
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+#else
+    int main(int argc, char *argv[])
+#endif
+    {
+        // Create application object
+        TutorialApplication app;
+
+        try {
+            app.go();
+        } catch(Ogre::Exception& e)  {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+            MessageBox(NULL, e.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+            std::cerr << "An exception has occurred: " <<
+                e.getFullDescription().c_str() << std::endl;
+#endif
+        }
+
+        return 0;
+    }
+
+#ifdef __cplusplus
+}
+#endif
+
+//---------------------------------------------------------------------------
